9b967a6dc1ebd72f420ee2589cedb1db4753205b
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2020-06-10 22:07:03 +0530

    Initial commit

---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUS"),
+    path("contact/", views.contact, name="ContactUS"),
+    path("tracker/", views.tracker, name="TrackingStatus"),
+    path("search/", views.search, name="Search"),
+    path("productview/", views.productView, name="ProductView"),
+    path("checkout/", views.checkout, name="checkout"),
+]
