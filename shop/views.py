0d9df0cd9d1cd615bfc5d3d5c173d08ff93b11af
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2020-06-12 11:50:51 +0530

    Fetch and display product details based on multiple category

--- shop/views.py
+++ shop/views.py
@@ -5,11 +5,20 @@ from math import ceil
 # Create your views here.
 
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    params = {'no_of_slides':nSlides, 'range':range(1,nSlides), 'product':products}
+    #products = Product.objects.all()
+    #print(products)
+    #n = len(products)
+    #nSlides = n//4 + ceil((n/4)-(n//4))
+    allprods = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n//4 + ceil((n/4)-(n//4))
+        allprods.append([prod, range(1, nSlides), nSlides])
+    #params = {'no_of_slides':nSlides, 'range':range(1,nSlides), 'product':products}
+    params = {'allprods':allprods}
     return render(request, 'shop/index.html', params)
 
 
